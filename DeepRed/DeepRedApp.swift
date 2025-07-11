e86c97421c0b1c224599292dee780c12accf20e3
Prince <prince@grey.pm>
2025-07-10 21:15:57 -0500

    Initial Commit

---
+++ DeepRed/DeepRedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepRedApp.swift
+//  DeepRed
+//
+//  Created by grey on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DeepRedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
